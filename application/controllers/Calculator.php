571244236c85d64d2cd712fe25215700b3c073e4
Grifin Kyle <grifin4kyle@cvsu.edu.ph>
2024-10-01 14:31:06 +0800

    Last Commit

--- application/controllers/Calculator.php
+++ application/controllers/Calculator.php
@@ -1,10 +1,9 @@
 <?php 
-defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 class Calculator extends CI_Controller {
 
-    public function index(): void {
+    public function index(){
         $this->load->view('calculator');
     }
 
